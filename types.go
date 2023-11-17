901b9bb43d92d829ab871867b98264a4a1e9d5ba
wesley-lewis <lewis.wes.23@gmail.com>
2023-11-17 14:30:29 +0530

    createdAt field added to account

--- types.go
+++ types.go
@@ -1,20 +1,29 @@
 package main
 
-import "math/rand"
+import (
+	"math/rand"
+	"time"
+)
 
 type Account struct {
-	ID        int    `json:"id"`
-	FirstName string `json:"firstname"`
-	LastName  string `json:"lastname"`
-	Number    int64  `json:"number"`
-	Balance   int64  `json:"balance"`
+	ID        int       `json:"id"`
+	FirstName string    `json:"firstname"`
+	LastName  string    `json:"lastname"`
+	Number    int64     `json:"number"`
+	Balance   int64     `json:"balance"`
+	CreatedAt time.Time `json:"createdAt"`
 }
 
 func NewAccount(firstName, lastName string) *Account {
 	return &Account{
-		ID:        1,
 		FirstName: firstName,
 		LastName:  lastName,
 		Number:    int64(rand.Intn(10000000)),
+		CreatedAt: time.Now().UTC(),
 	}
+}
+
+type CreateAccountRequest struct {
+	FirstName string `json:"firstName"`
+	LastName  string `json:"lastName"`
 }
