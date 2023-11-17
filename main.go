82f23738e749b8468a12b4d9c84e82ac30758365
wesley-lewis <lewis.wes.23@gmail.com>
2023-11-17 14:30:03 +0530

    connecting to db and booting up server

--- main.go
+++ main.go
@@ -1,6 +1,21 @@
 package main
 
+import (
+	"fmt"
+	"log"
+)
+
 func main() {
-	server := NewApiServer(":3000")
+	store, err := NewPostgresStore()
+	if err != nil {
+		log.Fatal(err)
+	}
+	if err := store.Init(); err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Printf("%+v\n", store)
+
+	server := NewApiServer(":3000", store)
 	server.Run()
 }
